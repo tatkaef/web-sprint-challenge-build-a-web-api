81c21ba2adbae9e328add1ad587484823a142439
Tatiana Efimova <tatka_ef@mail.ru>
2020-12-07 23:51:14 -0800

    package, index, server

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,3 @@
 // Write your "actions" router here!
+
+
--- index.js
+++ index.js
@@ -12,3 +12,9 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Go code!
 */
+
+const server = require("./api/server.js");
+const port = process.env.PORT || 4000;
+server.listen(port, () => {
+  console.log("\n*** Server Running on http://localhost:4000 ***\n");
+});
